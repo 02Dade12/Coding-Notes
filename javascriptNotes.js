33c386d4e98d5c1c9f7442c3a025e33f868792ae
Enrique Collazo <ecollazo1991@gmail.com>
2021-01-15 11:51:18 -0500

    updated notes

---
+++ javascriptNotes.js
@@ -0,0 +1,15 @@
+// Basic Javascript cansist of the following:
+
+// Variables, var "variableName" = 'x';
+var variableName = '';
+
+// Console Logs allow developers to test what they build as they build it
+// console.log(x);
+// "x" will come out in the console log
+console.log();
+
+// Prompts, Confirms, & Alerts; these are alert boxes that populate
+prompt();
+confirm();
+alert();
+
